a64ea47fa3988d6cd29f03d7b2fae21895ecfb57
pcancio <pcancio@gmail.com>
2021-12-13 19:00:15 -0500

    12.2.4 mid

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Barack99!',
+        database: 'election',
+    },
+    console.log('connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
